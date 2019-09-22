47f6f768014d0f6fae56e8839fa32bd247d0a08b
pemakhov <serhiy.pemakhov@gmail.com>
2019-09-22 23:19:55 +0300

    Finished ps2 warm-up part 2. Fixed issues ps1 warm-up and landing

--- ps2-javascript-regexp/warmup/js/script.js
+++ ps2-javascript-regexp/warmup/js/script.js
@@ -1,4 +1,5 @@
-/* Calculates the sum of eligible numbers from the range of a to b */
+/* --- 01 --- */
+/* Calculates the sum of eligible numbers from the range from a to b */
 const calcSum = (a, b) => {
   /* Numbers of a range ending in these digits are eligible */
   const goodNumbers = [2, 3, 7];
@@ -14,7 +15,7 @@ const calcSum = (a, b) => {
 };
 
 /* Puts the sum of eligible numbers into HTML */
-const handleClick = (a, b) => {
+const putSum = (a, b) => {
   let first = parseInt(a, 10);
   let last = parseInt(b, 10);
   /* Replace the numbers if first is bigger than the second */
@@ -27,3 +28,50 @@ const handleClick = (a, b) => {
   const result = calcSum(first, last);
   document.getElementById('magic_sum').innerHTML = `Magic sum is  ${result}`;
 };
+
+/* --- 02 --- */
+// a.
+const SECONDS_IN_MINUTE = 60;
+const SECONDS_IN_HOUR = 3600;
+
+const getHours = (seconds) => Math.floor(seconds / SECONDS_IN_HOUR);
+const substractHours = (seconds, hours) => seconds - (hours * SECONDS_IN_HOUR);
+const getMinutes = (seconds) => Math.floor(seconds / SECONDS_IN_MINUTE);
+const substractMinutes = (seconds, minutes) => seconds - (minutes * SECONDS_IN_MINUTE);
+const addLeadingZero = (someNumber) => {
+  if (someNumber.toString().length === 1) {
+    return `0${someNumber}`;
+  }
+  return `${someNumber}`;
+};
+
+const convertSeconds = (totalSeconds) => { 
+  let seconds = parseInt(totalSeconds, 10);
+  let hours = getHours(seconds);
+  seconds = substractHours(seconds, hours);
+  let minutes = getMinutes(seconds);
+  seconds = substractMinutes(seconds, minutes);
+  hours = addLeadingZero(hours);
+  minutes = addLeadingZero(minutes);
+  seconds = addLeadingZero(seconds);
+  document.getElementById('formatted_time').innerHTML = `${hours}:${minutes}:${seconds}`;
+};
+
+// b.
+const timePattern = /\d\d:[0-5]\d:[0-5]\d/;
+
+const countSeconds = (hours, minutes, seconds) => hours * SECONDS_IN_HOUR + minutes
+  * SECONDS_IN_MINUTE + seconds;
+
+const putSeconds = (givenTime) => {
+  if (!timePattern.test(givenTime)) {
+    document.getElementById('counted_seconds').innerHTML = 'Input doesn\'t comply with the pattern';
+    return;
+  }
+  const time = givenTime.split(':');
+  const hours = parseInt(time[0], 10);
+  const minutes = parseInt(time[1], 10);
+  const seconds = parseInt(time[2], 10);
+  document.getElementById('counted_seconds').innerHTML = countSeconds(hours, minutes, seconds);
+};
+/* --- 03 --- */
