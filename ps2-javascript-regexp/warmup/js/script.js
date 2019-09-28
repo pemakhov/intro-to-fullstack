05a9301f2865433a2350177c47a6d3882a28838d
pemakhov <serhiy.pemakhov@gmail.com>
2019-09-28 19:34:34 +0300

    Added comments into ps2 warm-up script.js file

--- ps2-javascript-regexp/warmup/js/script.js
+++ ps2-javascript-regexp/warmup/js/script.js
@@ -1,3 +1,4 @@
+/* Time constants */
 const MILLISECONDS_IN_SECOND = 1000;
 const SECONDS_IN_MINUTE = 60;
 const SECONDS_IN_HOUR = 3600;
@@ -5,6 +6,7 @@ const HOURS_IN_DAY = 24;
 const DAYS_IN_MONTH = 30.44;
 const MONTHS_IN_YEAR = 12;
 
+/* Patterns */
 const isNumberPattern = /^[0-9-]+$/;
 const isTimeHMSPattern = /^\d\d:[0-5]\d:[0-5]\d$/;
 const isValidDatePattern = /^(\d{4})-\d\d-\d\dT\d\d:\d\d(:\d\d$|$)/;
@@ -13,10 +15,16 @@ const isURLPattern = /((http|https):\/\/)((www(?=\.)).|)[a-zA-Z0-9][a-zA-Z0-9-]+
 const isIPPattern = /(((2[0-5][0-5])|1\d\d|\d\d|\d):){3}(2[0-5][0-5]|1\d\d|\d\d|\d)(,|$|\s)/;
 const isRegExPattern = /\/.+(\/(?=([gimsuy]|$)))([gimsuy]+|$)/;
 
+/* Tests if given matchingWord matches given pattern
+ *  matchingWord - word or phrase that shell be tested;
+ *  pattern - the pattern to check.
+ */
 const matchPattern = (matchingWord, pattern) => pattern.test(matchingWord);
 
 /* --- 01 --- */
-/* Calculates the sum of eligible numbers from the range from a to b */
+/* Calculates the sum of eligible numbers from the range from a to b
+ *  a, b - the beginning and the end of the range.
+ */
 const calcSum = (a, b) => {
   /* Numbers of a range ending in these digits are eligible */
   const goodNumbers = [2, 3, 7];
@@ -52,10 +60,25 @@ const putSum = (a, b) => {
 
 /* --- 02 --- */
 // a.
+/* Calculates the number of hours from an amount of seconds */
 const getHours = (seconds) => Math.floor(seconds / SECONDS_IN_HOUR);
+
+/* Calculates the remainder of seconds
+ *  seconds - the total amount of seconds;
+ *  hours - the number of hours to extract from amount of seconds
+ */
 const substractHours = (seconds, hours) => seconds - (hours * SECONDS_IN_HOUR);
+
+/* Calculates the number of minutes from an amount of seconds */
 const getMinutes = (seconds) => Math.floor(seconds / SECONDS_IN_MINUTE);
+
+/* Calculates the remainder of seconds
+ *  seconds - the total amount of seconds;
+ *  minutes - the number of minutes to extract from amount of seconds
+ */
 const substractMinutes = (seconds, minutes) => seconds - (minutes * SECONDS_IN_MINUTE);
+
+/* Adds the leading zero to one digit numbers */
 const addLeadingZero = (someNumber) => {
   if (someNumber.toString().length === 1) {
     return `0${someNumber}`;
@@ -63,6 +86,7 @@ const addLeadingZero = (someNumber) => {
   return `${someNumber}`;
 };
 
+/* Converts seconds into hh:mm:ss format and inserts into HTML */
 const convertSeconds = (totalSeconds) => { 
   if (!isNumberPattern.test(totalSeconds)) {
     document.getElementById('formatted_time').innerHTML = 'The input must be a number';
@@ -80,9 +104,11 @@ const convertSeconds = (totalSeconds) => {
 };
 
 // b.
+/* Calculates the number of seconds */
 const countSeconds = (hours, minutes, seconds) => hours * SECONDS_IN_HOUR + minutes
   * SECONDS_IN_MINUTE + seconds;
 
+/* Calculates seconds and inserts them into HTML */
 const putSeconds = (givenTime) => {
   if (!isTimeHMSPattern.test(givenTime)) {
     document.getElementById('counted_seconds').innerHTML = 'Input doesn\'t comply with the pattern';
@@ -96,32 +122,51 @@ const putSeconds = (givenTime) => {
 };
 
 /* --- 03 --- */
+/* Some more time constants */
 const secondsInDay = SECONDS_IN_HOUR * HOURS_IN_DAY;
 const secondsInMonth = secondsInDay * DAYS_IN_MONTH;
 const secondsInYear = secondsInMonth * MONTHS_IN_YEAR;
 
+/* Gets the number of some periods of time (hours, months, etc.)
+ *  totalSeconds - the total number of seconds;
+ *  secondsInPeriod - the number of seconds in one period (i.e. 60 seconds in one minute).
+ */
 const getNomberOfPeriods = (totalSeconds, secondsInPeriod) => Math.floor(totalSeconds
   / secondsInPeriod);
-const extractPeriod = (totalSeconds, nomberOfPeriods, secondsInPeriod) => totalSeconds
-  - nomberOfPeriods * secondsInPeriod;
 
+/* Calculates the remainder of seconds
+ *  totalSeconds - the total amount of seconds;
+ *  numberOfPeriods - the number of periods;
+ *  secondsInPeriod - the number of seconds in one period.
+ */
+const extractPeriod = (totalSeconds, numberOfPeriods, secondsInPeriod) => totalSeconds
+  - numberOfPeriods * secondsInPeriod;
+
+/* Calculates the period of time between two dates and puts it into HTML */
 const putTimePeriod = (d1, d2) => {
+  /* Check if dates are valid and return from the function if not */
   if (!isValidDatePattern.test(d1) || !isValidDatePattern.test(d1)) {
     document.getElementById('time_between_dates').innerHTML = 'Invalid date(s)';
     return;
   }
+  /* Create date objects */
   const firstDate = new Date(d1);
   const secondDate = new Date(d2);
-  // The whole time period in seconds
+  /* The whole time period in seconds */
   const totalSeconds = Math.abs(firstDate - secondDate) / MILLISECONDS_IN_SECOND;
+  /* Years in the period */
   const resultYears = getNomberOfPeriods(totalSeconds, secondsInYear);
   let leftSeconds = extractPeriod(totalSeconds, resultYears, secondsInYear);
+  /* Months in the period */
   const resultMonths = getNomberOfPeriods(leftSeconds, secondsInMonth);
   leftSeconds = extractPeriod(leftSeconds, resultMonths, secondsInMonth);
+  /* Days in the period */
   const resultDays = getNomberOfPeriods(leftSeconds, secondsInDay);
   leftSeconds = extractPeriod(leftSeconds, resultDays, secondsInDay);
+  /* Hours in the period */
   const resultHours = getNomberOfPeriods(leftSeconds, SECONDS_IN_HOUR);
   leftSeconds = extractPeriod(leftSeconds, resultHours, SECONDS_IN_HOUR);
+  /* Minutes in the period */
   const resultMinutes = getNomberOfPeriods(leftSeconds, SECONDS_IN_MINUTE);
   leftSeconds = extractPeriod(leftSeconds, resultMinutes, SECONDS_IN_MINUTE);
   leftSeconds = Math.round(leftSeconds);
@@ -131,11 +176,16 @@ const putTimePeriod = (d1, d2) => {
 };
 
 /* --- 04 --- */
+/* Opent and close HTML tags for the board elements */
 const boardHTML = ['<div class="board">', '</div>'];
 const rowHTML = ['<div class="board__row">', '</div>'];
 const darkCellHTML = '<div class="board__cell"></div>';
 const lightCellHTML = '<div class="board__cell board__cell--light"></div>';
 
+/* Draws one row
+ *  width - the number of cells in the row;
+ *  odity - the indicator of a color of the first cell.
+ */
 const constructRow = (width, odity) => {
   let row = rowHTML[0];
   let isOdd = odity;
@@ -151,6 +201,9 @@ const constructRow = (width, odity) => {
   return row;
 };
 
+/* Draws the board
+ *  width, height - the number of cells in a row and a column
+ */
 const constructBoard = (width, height) => {
   let board = boardHTML[0];
   let isOdd = true;
@@ -162,6 +215,7 @@ const constructBoard = (width, height) => {
   return board;
 };
 
+/* Draws a chess board according to the dimensions from the input */
 const drawBoard = (dimensions) => {
   if (!isBoardDimensionsPattern.test(dimensions)) {
     document.getElementById('chess_board').innerHTML = 'Invalid board dimensions';
@@ -174,6 +228,10 @@ const drawBoard = (dimensions) => {
 };
 
 /* --- 05 --- */
+/* Constructs the URL in HTML format.
+ * Substracts the "http://" or "http://" part from the URL text.
+ *  source - the source of the URL.
+ */
 const makeLink = (source) => {
   const link = source.trim();
   const linkName = (link.startsWith('http')) ? link.substring(link.indexOf(':') + 3) : link;
@@ -183,6 +241,7 @@ const makeLink = (source) => {
   return '';
 };
 
+/* Parses links and URLs, form HTML code for web-links and inserts them into HTML */
 const processURLAndIP = (linksList) => {
   let data = linksList.split(',');
   data = data.map((link) => makeLink(link));
@@ -191,11 +250,15 @@ const processURLAndIP = (linksList) => {
 };
 
 /* --- 06 --- */
+/* Splits the RegExp into two parts: RegExp and flags
+ *  returns the array where the 0 element contains RegExp and the 1-st - flags
+ */
 const splitRegEx = (reg) => {
   const indexOfSplitter = reg.lastIndexOf('/');
   return [reg.substring(1, indexOfSplitter), reg.substring(indexOfSplitter + 1)];
 };
 
+/* Creates a RegExp object from the text or a RegExp */
 const makeRegEx = (phrase) => {
   if (phrase === '') return '';
   if (isRegExPattern.test(phrase)) {
@@ -205,6 +268,7 @@ const makeRegEx = (phrase) => {
   return new RegExp(phrase, 'g');
 };
 
+/* Replaces phrases in the text with an appropriate HTML code */
 const replaceMatches = (text, matched, matchedWithTags) => {
   let textWithTags = text;
   for (let i = 0; i < matched.length; i += 1) {
