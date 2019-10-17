34e04cd32935bab75037f8c395c2fdd3d47ae095
pemakhov <serhiy.pemakhov@gmail.com>
2019-10-17 23:51:56 +0300

    ps3 select. Added default selector. Maded it slideup at click somewhere in window

--- ps3-jquery/select/js/script.js
+++ ps3-jquery/select/js/script.js
@@ -22,27 +22,25 @@ const friends = [
   },
 ];
 
-/* True if not default option is selected */
-let isSelected = false;
 /* Contains HTML of the selector before any option was chosen */
-const defaultSelectorHtml = '<div class="selection default">Select Friend<span></span></div>';
-const restoreDefaultHtml = '<div class="restore-default-bar bar-selector">Select Friend</div>';
+const defaultSelectorHtml = `<div class="selection default">Select Friend<span></span></div>`;
+const restoreDefaultHtml = '<div id="bar-0" class="restore-default-bar bar-selector hidden">Select Friend</div>';
 /* Contains HTML of currently chosen or default option */
 let selectorHtml = defaultSelectorHtml;
 /* Contains the option that was chosen the last time, or null */
 let currentBar = null;
+let currentBarIndex = '';
+let barId = 0;
 
 /* Sets all options. Places them into a container ("panel"). Returns
 * container with options.
 */
 const setOptions = (options) => {
   /* The container for options */
-  let panel = '<div class="panel">';
-  if (isSelected) {
-    panel = panel.concat(restoreDefaultHtml);
-  }
+  let panel = `<div class="panel">`;
+  panel = panel.concat(restoreDefaultHtml);
   /* Wrap each bar into HTML tags */
-  const bars = friends.map((item) => '<div class="bar bar-selector">'
+  const bars = friends.map((item) => `<div id="bar${barId++}" class="bar bar-selector">`
     .concat('<img class="avatar" src="').concat(item.avatarSrc).concat('"></img>')
     .concat(item.name).concat('</div>')
   );
@@ -58,39 +56,55 @@ const setSelector = (selector, options) => {
   selector.append(setOptions(friends));
 };
 
+function clickOnBar() {
+  selectorHtml = $(this).html();
+  $('.panel').slideUp(500);
+  $('.selection').html(`${selectorHtml}<span></span>`);
+  $('#dropdown').toggleClass('active');
+  /* CurrentBar can be equal null before at least one option was selected */
+  if (currentBar !== null) {
+    currentBar.removeClass('hidden');
+  }
+  currentBar = $(this);
+  currentBarIndex = currentBar.attr('id');
+  if (currentBarIndex === 'bar-0') {
+    $('.selection').html('Select Friend<span></span>');
+    /* Selection with class 'default' has light-grey color */
+    $('.selection').addClass('default');
+  } else {
+    isSelected = true;
+    $('.selection').removeClass('default');
+    $('.restore-default-bar').removeClass('hidden');
+  }
+  currentBar.addClass('hidden');
+};
+
+/* Toggle arrow direction and border color */
+function toggleArrow() {
+  $('#dropdown').toggleClass('active');
+}
+
+
 /* Launches all functions when the document is ready */
 $(document).ready(function() {
   /* Sets the selector (dropdown list of options) */
   setSelector($('#dropdown'), friends);
   /* Toggle the panel (list of options) */
   $('.selection').click(function() {
-    $('.panel').slideToggle(500);
-    /* Toggle the border color for folded and unfolded selector */
-    $('#dropdown').toggleClass('active');
+    $('.panel').slideToggle(500, toggleArrow);
   });
   /* By default the slider is unfolded, so I make it folded */
   $('.panel').slideUp(0);
-  /* Makes a bar selected: shovs it on the top */
-  $('.bar-selector').click(function() {
-    selectorHtml = $(this).html();
-    $('.panel').slideUp(500);
-    $('.selection').html(`${selectorHtml}<span></span>`);
-    $('#dropdown').toggleClass('active');
-    /* CurrentBar can be equal null before at least one option was selected */
-    if (currentBar !== null) {
-      currentBar.removeClass('hidden');
+  /* Makes a bar selected: displays it on the top */
+  $('.bar-selector').click(clickOnBar);
+  /* Slide up at click somewhere in the window */
+  $(window).click(function() {
+    if ($('#dropdown').hasClass('active')) {
+      $('.panel').slideUp(500, toggleArrow);
     }
-    currentBar = $(this);
-    console.log(`isSelected = ${isSelected}`);
-    console.log(currentBar.val());
-    console.log(defaultSelectorHtml);
-    if (currentBar.innerHTML === defaultSelectorHtml) {
-        isSelected = false;
-      } else {
-        isSelected = true;
-      }
-    currentBar.addClass('hidden');
-    /* Selection with class 'default' has light-grey color */
-    $('.selection').removeClass('default');
+  });
+  /* Prevent undesired slideup at cklick inside the select */
+  $('#dropdown').click(function(event){
+    event.stopPropagation();
   });
 });
