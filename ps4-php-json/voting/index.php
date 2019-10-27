da610a9cdfdbddef9d0f7ca823038f34bb63fd8a
pemakhov <serhiy.pemakhov@gmail.com>
2019-10-28 00:09:40 +0200

    ps4 - voting. Created form and started styling it.

---
+++ ps4-php-json/voting/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Best day of the week</title>
+    <link rel="stylesheet" href="css/theme.css">
+</head>
+<body>
+<div class="wrapper">
+    <h1>Which is the best day of a week?</h1>
+    <form action="results.php">
+        <label>Sunday
+            <input type="radio" name="day" value="Sunday">
+        </label>
+        <label>Monday
+            <input type="radio" name="day" value="Sunday">
+        </label>
+        <label>Tuesday
+            <input type="radio" name="day" value="Sunday">
+        </label>
+        <label>Wednesday
+            <input type="radio" name="day" value="Sunday">
+        </label>
+        <label>Tuesday
+            <input type="radio" name="day" value="Sunday">
+        </label>
+        <label>Friday
+            <input type="radio" name="day" value="Sunday">
+        </label>
+        <label>Saturday
+            <input type="radio" name="day" value="Sunday">
+        </label>
+        <input type="submit" name="bestDay" value="Vote!">
+    </form>
+</div>
+</body>
+</html>
