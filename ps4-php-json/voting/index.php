e671ff5f934129d176c300b50ea181b8aa97d703
pemakhov <serhiy.pemakhov@gmail.com>
2019-10-28 23:20:36 +0200

    ps4 voting - Vote results are read and written from/to the json file

--- ps4-php-json/voting/index.php
+++ ps4-php-json/voting/index.php
@@ -6,32 +6,33 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Best day of the week</title>
+    <link href="https://fonts.googleapis.com/css?family=Electrolize|Nanum+Myeongjo&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/theme.css">
 </head>
 <body>
 <h1>Which is the best day of the week?</h1>
 <div class="wrapper">
-    <form action="results.php">
+    <form action="results.php" method="post">
         <label>Sunday
             <input type="radio" name="day" value="Sunday">
         </label>
         <label>Monday
-            <input type="radio" name="day" value="Sunday">
+            <input type="radio" name="day" value="Monday">
         </label>
         <label>Tuesday
-            <input type="radio" name="day" value="Sunday">
+            <input type="radio" name="day" value="Tuesday">
         </label>
         <label>Wednesday
-            <input type="radio" name="day" value="Sunday">
+            <input type="radio" name="day" value="Wednesday">
         </label>
         <label>Tuesday
-            <input type="radio" name="day" value="Sunday">
+            <input type="radio" name="day" value="Tuesday">
         </label>
         <label>Friday
-            <input type="radio" name="day" value="Sunday">
+            <input type="radio" name="day" value="Friday">
         </label>
         <label>Saturday
-            <input type="radio" name="day" value="Sunday">
+            <input type="radio" name="day" value="Saturday">
         </label>
         <input type="submit" name="bestDay" value="Vote!">
     </form>
--- ps4-php-json/voting/results.php
+++ ps4-php-json/voting/results.php
@@ -1,2 +1,63 @@
 <?php
-echo 'Hello world';
+function getFileContent($fName)
+{
+    $file = fopen($fName, 'r+') or die('Unable to open file.');
+    $txt = fread($file, filesize($fName));
+    fclose($file);
+    return $txt;
+}
+
+if (isset($_POST['day'])) {
+    $choice = $_POST['day'];
+    $fName = 'vote-base.json';
+    if (file_exists($fName) && filesize($fName) > 0) {
+        $txt = getFileContent($fName);
+        $json = json_decode($txt, true);
+        if (!array_key_exists($choice, $json)) {
+            $json[$choice] = 0;
+        }
+    } else {
+        $json[$choice] = 0;
+    }
+    $json[$choice] += 1;
+    $txt = json_encode($json, true);
+    $file = fopen($fName, 'w+') or die('Unable to open file.');
+    fwrite($file, $txt);
+    fclose($file);
+}
+?>
+
+<head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {packages: ['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            // Define the chart to be drawn.
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Element');
+            data.addColumn('number', 'Percentage');
+            data.addRows([
+                ['Sunday', 55],
+                ['Monday', 11],
+                ['Tuesday', 22],
+                ['Wednesday', 101],
+                ['Thursday', 44],
+                ['Friday', 77],
+                ['Saturday', 66]
+            ]);
+
+            // Instantiate and draw the chart.
+            var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
+            chart.draw(data, null);
+        }
+    </script>
+</head>
+<body>
+
+
+
+<!-- Identify where the chart should be drawn. -->
+<div id="myPieChart" style="width: 1024px; height: 800px; margin: 0 auto;"/>
+</body>
