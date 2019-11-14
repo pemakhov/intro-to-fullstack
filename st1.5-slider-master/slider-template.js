6b4a44a1b71155779aac46cf85c3db79a503773b
Сергій Пемахов <pemakhov@kbp.radiy.com>
2019-11-14 18:23:29 +0200

    st1.5 Added highlighting of the current preview

--- st1.5-slider-master/slider-template.js
+++ st1.5-slider-master/slider-template.js
@@ -11,26 +11,62 @@ const IMAGES = [
     '?image=1039'
 ];
 
-const getPreviewHtml = () => {
-    let alt = 0;
-    return IMAGES.map(ending => '<li><img class="preview" src="' + API_URL + '/' + SMALL_SIZE + '/'
-        + ending + '" alt="' + (alt++) + '"></li>');
+const getPreviewImgSrcList = () => {
+    return IMAGES.map(ending => API_URL + '/' + SMALL_SIZE + '/' + ending);
 };
+
+const getPreviewHtmlList = () => {
+    let index = 0;
+    const liStart = '<li><img class="preview" src="';
+    const liEnd = '"></li>'
+    let result = getPreviewImgSrcList().map(src => 
+        '<li>' + 
+            '<img class="preview ' + (index++) + '" src="' + src + '" alt="' + index + '">' +
+        '</li>');
+    return result.reduce((accumulator, current) => accumulator.concat(current));
+}
+
+/* const getPreviewHtml = () => { */
+/*     let alt = 0; */
+/*     return IMAGES.map(ending => '<li><img class="preview" src="' + API_URL + '/' + SMALL_SIZE + '/' */
+/*         + ending + '" alt="' + (alt++) + '"></li>'); */
+/* }; */
 
 const smallSrcIntoBig = (src) => {
     return src.replace(SMALL_SIZE, BIG_SIZE);
 };
 
+const switchImgWithArrow = (right) => {
+    const previous = $('li.current img');
+    const previousIndex = previous.attr('alt');
+    const nextIndex = right ? previousIndex + 1 : previousIndex - 1;
+    const next = $('img.' + nextIndex);
+    console.log(nextIndex);
+    console.log(previous);
+    console.log(next);
+    previous.parent('li').removeClass('current');
+    next.parent('li').addClass('current');
+};
+
 $(document).ready(function () {
     const $previewContainer = $('.slider-previews');
-    const previewListHtml = getPreviewHtml();
-    for (const item of previewListHtml) {
-        $previewContainer.append(item);
-    }
+    $previewContainer.append(getPreviewHtmlList);
 
     let currentImageSrc = $('.slider-current img');
-    let $currentPrev = $('.preview');
-    $currentPrev.click(function () {
+    $('.preview').click(function () {
         currentImageSrc.attr('src', smallSrcIntoBig($(this).attr('src')));
+        $('li.current').removeClass('current');
+        $(this).parent('li').addClass('current');
+    });
+
+    $(document).keydown(function(e) {
+        switch(e.which) {
+            case 37: switchImgWithArrow(true);
+            break;
+            case 39: switchImgWithArrow(false);
+            break;
+            default: return;
+        }
+        e.preventDefault();
     });
 });
